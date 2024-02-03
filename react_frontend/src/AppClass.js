0b28a722cd7b6d8cbcd1dd979acb9f2146b332f1
Nanthachai.CS07 <botsafe002@gmail.com>
2024-02-03 12:34:47 +0700

    DEV: create func render

---
+++ react_frontend/src/AppClass.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class AppClass extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, World.</h1>
+      </div>
+    );
+  }
+}
+
